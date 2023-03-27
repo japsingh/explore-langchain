4e1d806899981420c0b6ae431cb8e0115b15ee90
japsingh <japneets@gmail.com>
2023-03-26 23:11:19 -0700

    Running version of charts

--- sql-main-with-charts.py
+++ sql-main-with-charts.py
@@ -1,4 +1,8 @@
+# import os
+# os.environ["LANGCHAIN_HANDLER"] = "langchain"
+import sys
 import streamlit as st
+import json
 from sqlalchemy import create_engine, MetaData
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
@@ -23,10 +27,11 @@ from langchain.tools.sql_database.tool import (
 )
 from pydantic import BaseModel, Extra, Field, validator
 from langchain.tools.base import BaseTool
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.set_page_config(page_title="Execute Database queries in natural language", page_icon=":robot:")
 st.header("Visibility and Insights using natural language")
-
 
 class ChartCreatorTool(BaseSQLDatabaseTool, BaseTool):
     """Tool for creating charts."""
@@ -34,23 +39,55 @@ class ChartCreatorTool(BaseSQLDatabaseTool, BaseTool):
     name = "create_pie_chart"
     description = """
     Input to this tool is a json object which contains the following json elements:
-    "total": the total value of pie chart,
-    then followed by N number of json elements signifying each sub value of a pie chart having the following json format:
+    N number of json elements signifying each sub value of a pie chart having the following json format:
+
     "name": Name of sub value,
     "value": sub value number which is lesser than total
 
     Output of this tool is a visual chart depicting the pie chart.
     """
 
-    def _run(self, json: str) -> str:
-        """Parse the input json, create the pie chart and return 'success', or return an error message."""
-        
-        print("Input json is ", json)
-        print(json)
-        return "success"
-
     async def _arun(self, query: str) -> str:
         raise NotImplementedError("ChartCreatorTool does not support async")
+
+    def parse_json(self, json_s: str) -> str:
+        return "success"
+
+    # def parse_json(self, json_s: str) -> str:
+    #     print("running create_pie_chart tool. json_str: " + json_s)
+    #     # print("typeof jsont_s is " + type(json_s))
+    #     try:
+    #         pie_chart_json = json.loads(str(json_s))
+    #         print("pie_chart_json is " + pie_chart_json)
+    #     except json.JSONDecodeError as errexc2:
+    #         error_str="Json decode error while parsing input json: "+json_s
+    #         print(error_str)
+    #         return error_str
+    #     except TypeError as errexc1:
+    #         error_str="Type error while parsing input json: "+json_s+", error: "
+    #         print(error_str)
+    #         tuples = sys.exc_info()
+    #         print(tuples)
+    #         return error_str
+    #     except ValueError as errexc:
+    #         error_str="Value error while parsing input json: "+json_s
+    #         print(error_str)
+    #         return error_str
+    #     except Exception as exc:
+    #         error_str="Exception while parsing input json: "+json_s
+    #         print(error_str)
+    #         return error_str
+    #     except:
+    #         error_str="Generic Exception while parsing input json: "+json_s
+    #         print(error_str)
+    #         return error_str
+    #     return "success"
+
+    def _run(self, json_s: str) -> str:
+        """Parse the input json, create the pie chart and return 'success', or return an error message."""
+        return self.parse_json(json_s)
+
+
 
 class LWAssistantToolkit(BaseToolkit):
     """Toolkit for interacting with SQL databases."""
@@ -108,6 +145,10 @@ def create_agent():
                     "mid": 'mid column provides machine id of the machine where the process is running. mid links machine table with other tables.',
                     "hostname": 'hostname column provides the machine name / host name of the machine",
                     "os": 'os column provides Operating system installed on the machine, it can be one of 'Linux' or 'Windows'
+                    """,
+        "cmdline": """
+                    "cmdline": 'cmdline column provides cmdline / commandline of a process. The cmdline includes process path, along with commandline parameters passed to that process
+                    "cmdline_hash": 'cmdline_hash column is a foreign key which links process table with cmdline table. This column helps find command line of a process.'
                    """
     }
     db = SQLDatabase(engine, custom_table_info=custom_table_info)
@@ -144,16 +185,44 @@ if (query_input):
         #st.write(query_output.extra_info['sql_query'])
         st.write(query_output["output"])
         intermediate_steps = query_output["intermediate_steps"]
-    except:
-        st.write("Can't get answer to the query")
+    except Exception as e:
+        st.write("Can't get answer to the query: "+str(e))
 
 if (intermediate_steps):
-    st.markdown("### Generated SQL")
     try:
-        final_sql = intermediate_steps[-1][0][1]
-        st.write(final_sql)
-    except:
-        st.write("Can't obtain SQL query")
+        final_step = intermediate_steps[-1][0][0]
+        print("final_step: "+final_step)
+        if final_step == "query_sql_db":
+            st.markdown("### Generated SQL")
+            final_sql = intermediate_steps[-1][0][1]
+            st.write(final_sql)
+        elif final_step == "create_pie_chart":
+            st.markdown("### Pie Chart")
+            json_str = intermediate_steps[-1][0][1]
+            json_str = json_str.replace("\'", "\"")
+            print("json_str: "+json_str)
+            pie_chart_json = json.loads(json_str)
+
+            print(pie_chart_json)
+            if pie_chart_json != None:
+                print("Input json is ", pie_chart_json)
+                print("typeof pie_chart_json ", type(pie_chart_json))
+                labels=[]
+                values=[]
+
+                for name in pie_chart_json:
+                    print(name)
+                    if pie_chart_json[name] != 0:
+                        labels.append(name + "(" + str(pie_chart_json[name]) + ")")
+                        values.append(pie_chart_json[name])
+
+                npvalues = np.array(values)
+                fig1, ax1 = plt.subplots()
+                ax1.pie(npvalues, labels=labels, startangle=90)
+                ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+                st.pyplot(fig1)
+    except Exception as e:
+        st.write("Can't obtain SQL query: " + str(e))
 
         # st.write(intermediate_steps)
         
